9eadb58ef25bd99f948389d4e0b7688474cf6c28
Evandro Reichert <evandroreichert07@gmail.com>
2023-11-27 14:33:10 -0300

    rota /voto adicionada e body parser

--- 003ProjetoUrnaEletronica/server.js
+++ 003ProjetoUrnaEletronica/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const fs = require('fs/promises')
 const path = require('path')
+const bodyParser  = require('body-parser')
+const { error } = require('console')
 
 const app = express()
 const port = 3000
@@ -10,6 +12,7 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.static(path.join(__dirname, "client")))
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html")
@@ -19,12 +22,34 @@ app.get('/cargainicial', (req, res) => {
     async function lerCandidatos() {
         const readCsv = await fs.readFile('config.csv', 'utf-8')
         const candidatos = readCsv.split(',')
-
-        console.log(candidatos);
         res.send(candidatos)
     }
 
     lerCandidatos()
+})
+
+
+app.post('/voto', async (req, res) => {
+    try {
+        let { rg, numeroCandidato, timeStamp } = req.body
+
+        let successMessage = {
+            "status": "200",
+            "mensagem": "Voto Registrado Com sucesso"
+        }
+        let errorMessage = {
+            "status": "500",
+            "mensagem": "Erro ao registrar voto, contate o administrador do sistema"
+        }
+
+        await fs.appendFile('votacao.csv', `${rg},${numeroCandidato},${timeStamp}`)
+
+        res.json(successMessage)
+    }
+    catch (error) {
+        console.error("Erro:", error)
+        res.json(errorMessage)
+    }
 })
 
 app.listen(port, () => {
